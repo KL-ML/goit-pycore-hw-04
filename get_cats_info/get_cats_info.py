1993d6e63c5666de25cf12bc8e2bebc86999dd5b
Natalya Karpova <knataly0513@gmail.com>
2024-06-19 14:54:28 +0300

    Додає варіант вирішення завдання

--- get_cats_info/get_cats_info.py
+++ get_cats_info/get_cats_info.py
@@ -1,31 +1,17 @@
 # Функція get_cats_info(path) має приймати один аргумент - шлях до текстового файлу (path).
+# Кожен рядок файлу має містити унікальний ідентифікатор кота, його ім'я та вік, розділені комою. 
 
+# 1 Варіант - В разі наявності пошкоджених строк в файлі виводить помилку
 import re
 
 def get_cats_info(path):
 
-    # інізіалізує список
-    cats_info = []
-
     try:
         # відкриває файл для читання
         with open(path, "r", encoding="utf-8") as file:
-
-            # читає кожну строку поки вони є
-            while True:
-                line = file.readline()
-                if not line:
-                    break
-
-                # відсікає пошкоджені або пусті строки
-                pattern = r"\w+\,+\w+\,+\d"
-                match = re.search(pattern, line, re.IGNORECASE)
-                if match:
-
-                    # перетворює строку у словник і додає до списку
-                    l = line.strip().split(',')
-                    one_cat_info = {'id' : l[0], 'name' : l[1], 'age' : l[2].removesuffix('\n')}
-                    cats_info.append(one_cat_info)
+            #  створює список з інформацією
+            lines = [l.strip().split(',') for l in file.readlines()]
+            cats_info = [{'id' : l[0], 'name' : l[1], 'age' : l[2]} for l in lines]
 
         # Якщо по закінченню роботи функції список пустий - повертає повідомлення
         if cats_info == []:
@@ -37,12 +23,60 @@ def get_cats_info(path):
     # Обробляє помилку відсутності файлу за вказаним шляхом, або вказаний шлях пошкоджений
     except FileNotFoundError:
         print("Не вдалося знайти файл.")
-        return cats_info
     
-    # Обробляє помилку коли замість файлу вказаний шлях до директорії
+    # Обробляє помилку коли замість файлу вказали шлях до директорії
     except IsADirectoryError:
         print("Не вдалося знайти файл.")
-        return cats_info
+    
+    # Обробляє помилку, коли в файлі є пуста або пошкоджена строка
+    except IndexError:
+        print("Файл пошкоджений")
+
+
+# 2 Варіант - відсікає пусті або пошкоджені строки в файлі і повертає результат по всім цілим строкам
+# import re
+
+# def get_cats_info(path):
+
+#     # інізіалізує список
+#     cats_info = []
+
+#     try:
+#         # відкриває файл для читання
+#         with open(path, "r", encoding="utf-8") as file:
+
+#             # читає кожну строку поки вони є
+#             while True:
+#                 line = file.readline()
+#                 if not line:
+#                     break
+
+#                 # відсікає пошкоджені або пусті строки
+#                 pattern = r"\w+\,+\w+\,+\d"
+#                 match = re.search(pattern, line, re.IGNORECASE)
+#                 if match:
+
+#                     # перетворює строку у словник і додає до списку
+#                     l = line.strip().split(',')
+#                     one_cat_info = {'id' : l[0], 'name' : l[1], 'age' : l[2]}
+#                     cats_info.append(one_cat_info)
+
+#         # Якщо по закінченню роботи функції список пустий - повертає повідомлення
+#         if cats_info == []:
+#             print("Файл пошкоджений або пустий.")
+
+#         # Повертає список словників з інформацією про котів
+#         return cats_info
+
+#     # Обробляє помилку відсутності файлу за вказаним шляхом, або вказаний шлях пошкоджений
+#     except FileNotFoundError:
+#         print("Не вдалося знайти файл.")
+#         return cats_info
+    
+#     # Обробляє помилку коли замість файлу вказаний шлях до директорії
+#     except IsADirectoryError:
+#         print("Не вдалося знайти файл.")
+#         return cats_info
     
 
 
